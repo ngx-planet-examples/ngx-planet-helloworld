42a193217da02a1a47e1548b412c11c19ccd7b24
Dakshinamurthy Karra <kd@subex.com>
2022-11-20 14:40:15 +0000

    Added an mfe-showborder button

---
+++ ngx-planet-demo-portal/src/app/show-mfeborder/show-mfeborder.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'portal-show-mfeborder',
+  templateUrl: './show-mfeborder.component.html',
+  styleUrls: ['./show-mfeborder.component.scss']
+})
+export class ShowMFEBorderComponent implements OnInit {
+
+  window = window ;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
